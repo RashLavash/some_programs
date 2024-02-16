f5d2c37d1afe6631a0e16eec50c0035b508389d4
RashLavash <rashlavash000@gmail.com>
2024-02-16 21:53:57 +0300

    финальный проект, авторизация, cbv, кастомная модель пользователя

---
+++ django_lessons/lesson_9/final_project/final/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('auth/', include(('users.urls', 'users'), namespace='users')),
+    path('posts/', include(('posts.urls', 'posts'), namespace='posts')),
+    path('admin/', admin.site.urls),
+]
