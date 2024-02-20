d44cbf5e75a99da2e6d0d258c6db95441e0d4f37
RashLavash <rashlavash000@gmail.com>
2024-02-20 19:11:01 +0300

    домашка: повторение

---
+++ django_lessons/home_lesson_4/home_edu_proj/posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import PostList, PostDetail
+
+
+urlpatterns = [
+    path('post_list/', PostList.as_view(), name='post_list'),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+]
